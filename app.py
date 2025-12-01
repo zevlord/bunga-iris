45292c77afc9f1e0672011c00b5dbdda87fdaea9
zevlord <106267288+zevlord@users.noreply.github.com>
2025-12-01 15:28:51 +0700

    Fix indentation and variable name in app.py

--- app.py
+++ app.py
@@ -24,9 +24,10 @@ with col2:
 st.text('')
 if st.button("Predict type of Iris"):
     result = predict(
-        np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
+        np.array([[sepal_1, sepal_w, petal_l, petal_w]]))
     st.text(result[0])
 
 st.text('')
 st.text('')
+
 st.markdown('')
